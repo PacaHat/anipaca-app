128e6874b25bf62bbe902379a273d5c2ec1977c5
Raisul Islam <118072581+PacaHat@users.noreply.github.com>
2025-01-02 13:25:42 +0600

    Update changelogs.data.ts

--- website/src/.vitepress/theme/data/changelogs.data.ts
+++ website/src/.vitepress/theme/data/changelogs.data.ts
@@ -12,8 +12,8 @@ export { data }
 export default defineLoader({
   async load(): Promise<GitHubReleaseList> {
     const releases = await octokit.paginate(octokit.repos.listReleases, {
-      owner: 'aniyomiorg',
-      repo: 'aniyomi',
+      owner: 'PacaHat',
+      repo: 'Anipaca',
       per_page: 100,
     })
 
